204a1035f28e51c730c48ff7ac3af9e8c1508a17
Kanit Mann <kanitmann90@gmail.com>
2021-09-04 00:43:01 +0530

    :green_circle: Array Reversal

--- Question List/6.cpp
+++ Question List/6.cpp
@@ -18,3 +18,39 @@ Input :  arr[] = {4, 5, 1, 2}
 Output : arr[] = {2, 1, 5, 4}
 
 */
+
+#include <iostream>
+
+using namespace std;
+
+int rev_arr(int arr[], int m, int n)
+{
+    int temp = 0;
+    while (m < n)
+    {
+        temp = arr[m];
+        arr[m] = arr[n];
+        arr[n] = temp;
+        m++;
+        n--;
+    }
+    return 0;
+}
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+
+    printf("\n");
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    rev_arr(arr, 0, n - 1);
+    printArray(arr, n);
+    return 0;
+}

472d38d85a55f3d5cc5ee22683e5dcfe3183133c
kanitmann <kanitmann90@gmail.com>
2021-12-08 13:12:52 -0500

    :green_circle: Alternate Swap added

---
+++ Leetcode/Alternate_Swap.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int printArr(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+    return 0;
+}
+
+int swap(int &a, int &b)
+{
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    return 0;
+}
+
+int swapAlternate(int arr[], int size)
+{
+    for (int i = 0; i < size; i = i + 2)
+    {
+        if ((i + 1) < size)
+        {
+            swap(arr[i], arr[i + 1]);
+        }
+    }
+    printArr(arr, size);
+    return 0;
+}
+
+int main()
+{
+
+    int even[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int odd[7] = {5, 6, 7, 8, 9, 10, 11};
+    swapAlternate(even, 8);
+    swapAlternate(odd, 7);
+    return 0;
+}
